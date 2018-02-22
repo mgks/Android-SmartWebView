d3a789a7e233bc1f2a6ec1a7d6409410ebedf99a
Ghazi Khan <getmgks@gmail.com>
2018-02-22 15:40:49 +0530

    Rating System Settings

--- app/src/main/java/mgks/os/webview/AppRate.java
+++ app/src/main/java/mgks/os/webview/AppRate.java
@@ -1,7 +1,9 @@
 package mgks.os.webview;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.view.View;
 
 import java.util.Date;
@@ -16,7 +18,8 @@ import static mgks.os.webview.PreferenceHelper.setInstallDate;
 
 public final class AppRate {
 
-    private static AppRate singleton;
+    @SuppressLint("StaticFieldLeak")
+	private static AppRate singleton;
 
     private final Context context;
 
@@ -45,29 +48,28 @@ public final class AppRate {
         return singleton;
     }
 
-    public static boolean showRateDialogIfMeetsConditions(Activity activity) {
+    static void showRateDialogIfMeetsConditions(Activity activity) {
         boolean isMeetsConditions = singleton.isDebug || singleton.shouldShowRateDialog();
         if (isMeetsConditions) {
             singleton.showRateDialog(activity);
         }
-        return isMeetsConditions;
-    }
+	}
 
     private static boolean isOverDate(long targetDate, int threshold) {
         return new Date().getTime() - targetDate >= threshold * 24 * 60 * 60 * 1000;
     }
 
-    public AppRate setLaunchTimes(int launchTimes) {
+    AppRate setLaunchTimes(int launchTimes) {
         this.launchTimes = launchTimes;
         return this;
     }
 
-    public AppRate setInstallDays(int installDate) {
+    AppRate setInstallDays(int installDate) {
         this.installDate = installDate;
         return this;
     }
 
-    public AppRate setRemindInterval(int remindInterval) {
+    AppRate setRemindInterval(int remindInterval) {
         this.remindInterval = remindInterval;
         return this;
     }
@@ -123,7 +125,7 @@ public final class AppRate {
         return this;
     }
 
-    public AppRate setMessage(int resourceId) {
+    AppRate setMessage(int resourceId) {
         options.setMessageResId(resourceId);
         return this;
     }
@@ -133,7 +135,7 @@ public final class AppRate {
         return this;
     }
 
-    public AppRate setTextRateNow(int resourceId) {
+    AppRate setTextRateNow(int resourceId) {
         options.setTextPositiveResId(resourceId);
         return this;
     }
@@ -143,7 +145,7 @@ public final class AppRate {
         return this;
     }
 
-    public AppRate setTextLater(int resourceId) {
+    AppRate setTextLater(int resourceId) {
         options.setTextNeutralResId(resourceId);
         return this;
     }
@@ -153,7 +155,7 @@ public final class AppRate {
         return this;
     }
 
-    public AppRate setTextNever(int resourceId) {
+    AppRate setTextNever(int resourceId) {
         options.setTextNegativeResId(resourceId);
         return this;
     }
@@ -168,25 +170,25 @@ public final class AppRate {
         return this;
     }
 
-    public AppRate setStoreType(StoreType appstore) {
+    AppRate setStoreType(StoreType appstore) {
         options.setStoreType(appstore);
         return this;
     }
 
-    public void monitor() {
+    void monitor() {
         if (isFirstLaunch(context)) {
             setInstallDate(context);
         }
         PreferenceHelper.setLaunchTimes(context, getLaunchTimes(context) + 1);
     }
 
-    public void showRateDialog(Activity activity) {
+    private void showRateDialog(Activity activity) {
         if (!activity.isFinishing()) {
             create(activity, options).show();
         }
     }
 
-    public boolean shouldShowRateDialog() {
+    private boolean shouldShowRateDialog() {
         return getIsAgreeShowDialog(context) &&
                 isOverLaunchTimes() &&
                 isOverInstallDate() &&
@@ -213,5 +215,93 @@ public final class AppRate {
         this.isDebug = isDebug;
         return this;
     }
+
+}
+final class PreferenceHelper {
+
+	private static final String PREF_FILE_NAME = "android_rate_pref_file";
+
+	private static final String PREF_KEY_INSTALL_DATE = "android_rate_install_date";
+
+	private static final String PREF_KEY_LAUNCH_TIMES = "android_rate_launch_times";
+
+	private static final String PREF_KEY_IS_AGREE_SHOW_DIALOG = "android_rate_is_agree_show_dialog";
+
+	private static final String PREF_KEY_REMIND_INTERVAL = "android_rate_remind_interval";
+
+	private PreferenceHelper() {
+	}
+
+	private static SharedPreferences getPreferences(Context context) {
+		return context.getSharedPreferences(PREF_FILE_NAME, Context.MODE_PRIVATE);
+	}
+
+	private static SharedPreferences.Editor getPreferencesEditor(Context context) {
+		return getPreferences(context).edit();
+	}
+
+	/**
+	 * Clear data in shared preferences.<br/>
+	 *
+	 * @param context context
+	 */
+	static void clearSharedPreferences(Context context) {
+		SharedPreferences.Editor editor = getPreferencesEditor(context);
+		editor.remove(PREF_KEY_INSTALL_DATE);
+		editor.remove(PREF_KEY_LAUNCH_TIMES);
+		editor.apply();
+	}
+
+	/**
+	 * Set agree flag about show dialog.<br/>
+	 * If it is false, rate dialog will never shown unless data is cleared.
+	 *
+	 * @param context context
+	 * @param isAgree agree with showing rate dialog
+	 */
+	static void setAgreeShowDialog(Context context, boolean isAgree) {
+		SharedPreferences.Editor editor = getPreferencesEditor(context);
+		editor.putBoolean(PREF_KEY_IS_AGREE_SHOW_DIALOG, isAgree);
+		editor.apply();
+	}
+
+	static boolean getIsAgreeShowDialog(Context context) {
+		return getPreferences(context).getBoolean(PREF_KEY_IS_AGREE_SHOW_DIALOG, true);
+	}
+
+	static void setRemindInterval(Context context) {
+		SharedPreferences.Editor editor = getPreferencesEditor(context);
+		editor.remove(PREF_KEY_REMIND_INTERVAL);
+		editor.putLong(PREF_KEY_REMIND_INTERVAL, new Date().getTime());
+		editor.apply();
+	}
+
+	static long getRemindInterval(Context context) {
+		return getPreferences(context).getLong(PREF_KEY_REMIND_INTERVAL, 0);
+	}
+
+	static void setInstallDate(Context context) {
+		SharedPreferences.Editor editor = getPreferencesEditor(context);
+		editor.putLong(PREF_KEY_INSTALL_DATE, new Date().getTime());
+		editor.apply();
+	}
+
+	static long getInstallDate(Context context) {
+		return getPreferences(context).getLong(PREF_KEY_INSTALL_DATE, 0);
+	}
+
+	static void setLaunchTimes(Context context, int launchTimes) {
+		SharedPreferences.Editor editor = getPreferencesEditor(context);
+		editor.putInt(PREF_KEY_LAUNCH_TIMES, launchTimes);
+		editor.apply();
+	}
+
+	static int getLaunchTimes(Context context) {
+		return getPreferences(context).getInt(PREF_KEY_LAUNCH_TIMES, 0);
+	}
+
+	static boolean isFirstLaunch(Context context) {
+		return getPreferences(context).getLong(PREF_KEY_INSTALL_DATE, 0) == 0L;
+	}
 
 }
