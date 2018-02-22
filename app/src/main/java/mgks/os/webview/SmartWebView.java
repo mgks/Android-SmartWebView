24d7b7939e66ed232f8918f7fd7c50de733a58a1
Ghazi Khan <getmgks@gmail.com>
2018-02-22 16:49:42 +0530

    Customise Smart WebView Variables

--- app/src/main/java/mgks/os/webview/SmartWebView.java
+++ app/src/main/java/mgks/os/webview/SmartWebView.java
@@ -20,15 +20,15 @@ class SmartWebView {
 	static boolean ASWP_PBAR        = true;     //show progress bar in app
 	static boolean ASWP_ZOOM        = false;    //zoom control for webpages view
 	static boolean ASWP_SFORM       = false;    //save form cache and auto-fill information
-	static boolean ASWP_OFFLINE		= false;	//whether the loading webpages are offline or online
-	static boolean ASWP_EXTURL		= true;		//open external url with default browser instead of app webview
+	static boolean ASWP_OFFLINE     = false;    //whether the loading webpages are offline or online
+	static boolean ASWP_EXTURL      = true;     //open external url with default browser instead of app webview
 
 	//Configuration variables
-	static String ASWV_URL      	= "http://mgks.infeeds.com/"; //complete URL of your website or webpage
-	static String ASWV_F_TYPE   	= "*/*";  //to upload any file type using "*/*"; check file type references for more
+	static String ASWV_URL          = "http://mgks.infeeds.com/"; //complete URL of your website or webpage
+	static String ASWV_F_TYPE       = "*/*";  //to upload any file type using "*/*"; check file type references for more
 
 	//Rating system variables
-	static int ASWR_DAYS			= 3;		//after how many days of usage would you like to show the dialoge
-	static int ASWR_TIMES			= 10;		//overall request launch times being ignored
-	static int ASWR_INTERVAL		= 2;		//reminding users to rate after days interval
+	static int ASWR_DAYS            = 3;        //after how many days of usage would you like to show the dialoge
+	static int ASWR_TIMES           = 10;       //overall request launch times being ignored
+	static int ASWR_INTERVAL        = 2;        //reminding users to rate after days interval
 }
