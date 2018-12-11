b3a460a2b5b6698eeea2623049f6539e2d747efa
mgks <getmgks@gmail.com>
2018-12-11 19:01:38 +0530

    Smart WebView Image Preview Update

--- app/src/main/res/raw/script.js
+++ app/src/main/res/raw/script.js
@@ -1,16 +1,20 @@
-function readURL(e){if(e.files&&e.files[0]){var r=new FileReader;r.onload=function(e){$("#impr").removeClass("hidden"),$("#impr").attr("src",e.target.result)},r.readAsDataURL(e.files[0])}}
-function getUrlVars(){
-    var vars = [], hash;
-    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
-    for(var i = 0; i < hashes.length; i++)
-    {
-        hash = hashes[i].split('=');
-        vars.push(hash[0]);
-        vars[hash[0]] = hash[1];
-    }
-    return vars;
-}
 $(document).ready(function(){
+    var imagesPreview = function(input, placeToInsertImagePreview) {
+        if (input.files) {
+            var filesAmount = input.files.length;
+            for (i = 0; i < filesAmount; i++) {
+                var reader = new FileReader();
+                reader.onload = function(event) {
+                    $($.parseHTML('<img>')).attr('src', event.target.result).appendTo(placeToInsertImagePreview);
+                }
+                reader.readAsDataURL(input.files[i]);
+            }
+        }
+    };
+    $('#gallery-photo-add').on('change', function() {
+    	$(".gallery").html("");
+        imagesPreview(this, 'div.gallery');
+    });
 	var loc = getUrlVars()["loc"].split(',');
 	$(".locf").replaceWith("Latitude: "+loc[0]+"<br>Longitude: "+loc[1]);
 })
