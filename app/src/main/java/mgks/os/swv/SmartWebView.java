4324b46c9d44223d38bac615e058a01a47d56a4a
Ghazi Khan <getmgks@gmail.com>
2020-11-08 02:42:34 +0530

    custom user agent variables

--- app/src/main/java/mgks/os/swv/SmartWebView.java
+++ app/src/main/java/mgks/os/swv/SmartWebView.java
@@ -18,7 +18,7 @@ class SmartWebView {
 	static boolean ASWP_ONLYCAM       = false;        // incase you want only camera files to upload
 	static boolean ASWP_MULFILE       = true;         // upload multiple files in webview
 	static boolean ASWP_LOCATION      = true;         // track GPS locations
-	static boolean ASWP_CP			  = false;		  // enable copy/paste within webview
+	static boolean ASWP_CP            = false;        // enable copy/paste within webview
 
 	static boolean ASWP_RATINGS       = true;         // show ratings dialog; auto configured ; edit method get_rating() for customizations
 
@@ -32,7 +32,7 @@ class SmartWebView {
 	static boolean ASWP_TAB           = true;         // instead of default browser, open external URLs in chrome tab
 	static boolean ASWP_ADMOB         = true;         // to load admob or not
 
-	static boolean ASWP_EXITDIAL	  = true;		  // confirm to exit app on back press
+	static boolean ASWP_EXITDIAL	  = true;         // confirm to exit app on back press
 
 	/* -- SECURITY VARIABLES -- */
 	static boolean ASWP_CERT_VERI     = true;         // verify whether HTTPS port needs certificate verification
@@ -43,12 +43,18 @@ class SmartWebView {
 	static int ASWV_LAYOUT            = 0;            // default=0; for clear fullscreen layout, and =1 for drawer layout
 
 	// URL configs
-	static String ASWV_URL            = "https://apps.mgks.dev/swv/?android=true";     // complete URL of your website or offline webpage "file:///android_asset/offline.html";
-	static String ASWV_SEARCH         = "https://www.google.com/search?q=";       // search query will start by the end of the present string
-	static String ASWV_SHARE_URL      = ASWV_URL + "?share=";                     // URL where you process external content shared with the app
+	static String ASWV_URL            = "https://apps.mgks.dev/swv/?android=true";  // complete URL of your website or offline webpage "file:///android_asset/offline.html";
+	static String ASWV_SEARCH         = "https://www.google.com/search?q=";         // search query will start by the end of the present string
+	static String ASWV_SHARE_URL      = ASWV_URL + "?share=";                       // URL where you process external content shared with the app
 
 	// domains allowed to be opened inside webview
-	static String ASWV_EXC_LIST       = "github.com";				//separate domains with a comma (,)
+	static String ASWV_EXC_LIST       = "github.com";       //separate domains with a comma (,)
+
+	// custom user agent defaults
+	static boolean POSTFIX_USER_AGENT       = true;         // set to true to append USER_AGENT_POSTFIX to user agent
+	static boolean OVERRIDE_USER_AGENT      = true;         // set to true to use USER_AGENT instead of default one
+	static String USER_AGENT_POSTFIX        = "AndroidApp"; // useful for identifying traffic, e.g. in Google Analytics
+	static String CUSTOM_USER_AGENT         = "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Mobile Safari/537.36";
 
 	// to upload any file type using "*/*"; check file type references for more
 	static String ASWV_F_TYPE         = "*/*";
@@ -61,11 +67,5 @@ class SmartWebView {
 	static int ASWR_DAYS      = 3;            // after how many days of usage would you like to show the dialog
 	static int ASWR_TIMES     = 10;           // overall request launch times being ignored
 	static int ASWR_INTERVAL  = 2;            // reminding users to rate after days interval
-	
-	// User Agent tweaks
-	static boolean POSTFIX_USER_AGENT = true; // set to true to append USER_AGENT_POSTFIX to user agent
-	static boolean OVERRIDE_USER_AGENT = true; // set to true to use USER_AGENT instead of default one
-	static String USER_AGENT_POSTFIX = "AndroidApp"; // useful for identifying traffic, e.g. in Google Analytics
-	static String USER_AGENT = "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Mobile Safari/537.36";
 
 }
