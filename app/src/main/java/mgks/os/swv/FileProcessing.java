b12e9bd8d3168d4ee2e15863e4cfff7231c98d55
mgks <hello@mgks.dev>
2025-01-03 16:35:05 +0530

    smart webview v7.0

--- app/src/main/java/mgks/os/swv/FileProcessing.java
+++ app/src/main/java/mgks/os/swv/FileProcessing.java
@@ -1,5 +1,21 @@
 package mgks.os.swv;
 
+/*
+  Smart WebView 7.0
+
+  MIT License (https://opensource.org/licenses/MIT)
+
+  Smart WebView is an Open Source project that integrates native features into
+  WebView to help create advanced hybrid applications (https://github.com/mgks/Android-SmartWebView).
+
+  Explore plugins and enhanced capabilities: (https://mgks.dev/app/smart-webview-documentation#plugins)
+  Join the discussion: (https://github.com/mgks/Android-SmartWebView/discussions)
+  Support Smart WebView: (https://github.com/sponsors/mgks)
+
+  Your support and acknowledgment of the project's source are greatly appreciated.
+  Giving credit to developers encourages them to create better projects.
+*/
+
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.ClipData;
